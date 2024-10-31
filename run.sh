f7aeb6b3454dfd9ccf108c4084f2f81036934eb8
Samuel Tan <45391854+PeasantL@users.noreply.github.com>
2024-10-31 21:48:55 +0800

    added run scripts

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Check if .venv exists, if not, create it
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run main.py
+python main.py
